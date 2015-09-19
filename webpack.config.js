6159b1b2fa2efb37050b66c8e9eb38326dea6b08
Bruce Coddington <brucecoddington@gmail.com>
2015-09-18 19:44:09 -0500

    moved semantic into assets added redux libraries added redux universal example components and web pack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,33 @@
 var path = require('path');
 var webpack = require('webpack');
+var host = 'localhost';
+var port = 3001;
 
 module.exports = {
   watch: true,
+  devServerPort: port,
+  devtool: 'inline-source-map',
 
   entry: [
+    'webpack-dev-server/client?http://' + host + ':' + port,
+    'webpack/hot/only-dev-server',
     "./app/client.js"
   ],
 
   output: {
     path: __dirname + '/dist/assets/js',
-    filename: "app.js"
+    filename: "app.js",
+    publicPath: 'http://' + host + ':' + port + '/dist/'
   },
 
   module: {
     loaders: [
       {
         test: /\.js$/,
+        loaders: ['babel?optional=runtime'],
         include: [
           path.resolve(__dirname, 'app')
-        ],
-        loader: 'babel-loader'
+        ]
       }
     ]
   },
@@ -32,6 +39,7 @@ module.exports = {
   },
 
   plugins: [
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ]
 };
